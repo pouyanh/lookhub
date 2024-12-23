a873eba4e1c57868cd431e341fdc0112a2d75385
Pouyan Heyratpour <me@pouyan.dev>
2024-12-23 04:41:04 +0330

    feat: more domain repo queries
    * join get unexpired domains with resource records
    * create domain
    * add resource record
    
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- dnslv/adapters/db/dnslv.sql
+++ dnslv/adapters/db/dnslv.sql
@@ -1,6 +1,27 @@
--- DOMAINS QUERIES
+-- DOMAIN AGGREGATE
 
--- name: GetUnexpiredDomain :one
-SELECT *
+-- name: GetUnexpiredDomain :many
+SELECT d.id     AS domain_id,
+			 d.name   AS domain_name,
+			 rr.id    AS resource_record_id,
+			 rr.type  AS resource_record_type,
+			 rr.value AS resource_record_value,
+			 rr.ttl   AS resource_record_ttl
+FROM domains d
+			 LEFT JOIN resource_records rr ON d.id = rr.domain_id
+WHERE NOW() - (INTERVAL '1 second' * @ttl::INT) <= d.updated_at;
+
+-- name: DomainExists :one
+SELECT id
 FROM domains
-WHERE NOW() - (@ttl::INT * INTERVAL '1 second') > updated_at;
+WHERE name = @name;
+
+-- name: CreateDomain :one
+INSERT INTO domains (name)
+VALUES (@name)
+RETURNING *;
+
+-- name: AddResourceRecord :batchmany
+INSERT INTO resource_records (domain_id, type, value, ttl)
+VALUES (@domain_id, @type, @value, @ttl)
+RETURNING *;
