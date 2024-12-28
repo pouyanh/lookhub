ace8c2103bb4da6b8f7e2eae7e887fa5576b64ac
Pouyan Heyratpour <me@pouyan.dev>
2024-12-28 13:42:33 +0330

    build/platform: moved etcd backup cronjob to yaml
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- .deployment/platform/backup.tf
+++ .deployment/platform/backup.tf
@@ -1,50 +1,5 @@
 resource "kubernetes_manifest" "etcd_backup_cronjob" {
-	manifest = yamldecode(<<-EOT
-    apiVersion: batch/v1
-    kind: CronJob
-    metadata:
-      name: etcd-backup
-      namespace: kube-system
-    spec:
-      schedule: "0 * * * *"
-      jobTemplate:
-        spec:
-          template:
-            spec:
-              tolerations:
-                - key: "node-role.kubernetes.io/control-plane"
-                  operator: "Exists"
-                  effect: "NoSchedule"
-              containers:
-              - name: etcd-backup
-                image: bitnami/etcd:latest
-                command:
-                - /bin/sh
-                - -c
-                - |
-                  export ETCDCTL_API=3
-                  etcdctl snapshot save /etcd-backups/etcd-snapshot-$(date +%Y%m%d%H%M%S).db \
-                    --endpoints=https://127.0.0.1:2379 \
-                    --cacert=/certs/ca.crt \
-                    --cert=/certs/server.crt \
-                    --key=/certs/server.key
-                volumeMounts:
-                - name: etcd-certs
-                  mountPath: /certs
-                  readOnly: true
-                - name: etcd-backups
-                  mountPath: /etcd-backups
-              restartPolicy: OnFailure
-              volumes:
-              - name: etcd-certs
-                hostPath:
-                  path: /etc/kubernetes/pki/etcd
-                  type: DirectoryOrCreate
-              - name: etcd-backups
-                hostPath:
-                  path: /backups/etcd
-    EOT
-	)
+	manifest = yamldecode(file("${path.module}/backup_etcd.yaml"))
 
 	depends_on = [kind_cluster.default]
 }
