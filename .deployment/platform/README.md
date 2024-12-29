744283030307c2106db0a0c9f5617170bd29919e
Pouyan Heyratpour <me@pouyan.dev>
2024-12-29 12:17:27 +0330

    ci: argo debug mode
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

---
+++ .deployment/platform/README.md
@@ -0,0 +1,2 @@
+# Local Kubernetes Cluster
+It's build using Terraform and KinD
--- .gitlab/ci/scripts/03-00-deploy-argo.sh
+++ .gitlab/ci/scripts/03-00-deploy-argo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 argocd login "${ARGO_ENDPOINT}" --auth-token "${ARGO_TOKEN}" --insecure
 argocd app sync "${CI_PROJECT_NAME}"
 argocd app wait "${CI_PROJECT_NAME}" --health --timeout 300
